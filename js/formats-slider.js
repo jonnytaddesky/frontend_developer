03dcc52bb97299b7f15367511960a7e09e57e59f
jonnytaddesky <s.v.artem2020@gmail.com>
2023-12-07 23:08:03 +0200

    add optimize mobile

---
+++ js/formats-slider.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".carousel-button.next",
+    prevEl: ".carousel-button.prev",
+  },
+});
